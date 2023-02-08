4622ae4285a9750e60054cf0fb36be587ef7c4cc
Dfolli <dfolli@iissgadda.it>
2023-02-08 09:48:07 +0100

    salvo file aggiunti

---
+++ file2.js
@@ -0,0 +1,10 @@
+( function () {
+    var c = 0;
+    var m = Math.floor( 10000 * Math.random() );
+    for ( var a=0; a<m; a++)
+        for ( var b=a; b<10000; b++ )
+            c = a * b - a + b;
+        
+    console.log('Fine esecuzione script esterno #2 - m: ' + m);
+}
+)();
